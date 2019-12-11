eaf041b2f31f8a0466dc91836941690cee88ec35
nihaprezz <sergio13prez@gmail.com>
2019-12-11 11:23:00 -0500

    adding figaro gem and rest client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::API
     before_action :authorized
+
+    SECRET_KEY = ENV['secret_key']  #NOT SURE IF THIS WILL WORK, IF NOT PULL OUT OF APPLICATION.YML FILE
     
     def encode_token(payload)
-        JWT.encode(payload, 'my_s3cr3t')
+        JWT.encode(payload, SECRET_KEY)
     end
 
     def auth_header
@@ -13,7 +15,7 @@ class ApplicationController < ActionController::API
         if auth_header
             token = auth_header.split(' ')[1]
             begin
-              JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+              JWT.decode(token, SECRET_KEY, true, algorithm: 'HS256')
             rescue JWT::DecodeError
               nil
             end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :username, uniqueness: { case_sensitive: false }
 
-
+    #user can be a a main user or a friend
 end
